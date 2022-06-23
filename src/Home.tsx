d7087e96c05c1e10a10e4a75aea28a337dbc321e
little projects <littleratcc0@gmail.com>
2022-06-23 21:34:03 +0200

    FIX

--- src/Home.tsx
+++ src/Home.tsx
@@ -689,7 +689,7 @@ const Home = (props: HomeProps) => {
                                 renderer={renderEndDateCounter}
                               />}
                             {wallet && isActive &&
-                              <h3>TOTAL MINTED : 90 / {itemsAvailable}</h3>}
+                              <h3>TOTAL MINTED : 110 / {itemsAvailable}</h3>}
                             {wallet && isActive && <BorderLinearProgress variant="determinate"
                                                                          value={100 - (itemsRemaining * 100 / itemsAvailable)}/>}
                             <br/>
